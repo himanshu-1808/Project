39ffad7b96fbf95ab522637c4f90ce6684a4a9eb
Himanshu <himanshu1112.cse19@chitkara.edu.in>
2022-01-29 20:50:23 +0530

    Js file added

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Tempapp from './components/TempApp';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Tempapp/>
   );
 }
 
---
+++ weather/src/components/TempApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./css/style.css";
+
+const Tempapp= ()=>{
+    return(
+        <div className="box">
+         <div className="inputData">
+             <input type="search" className="inputField" onChange={(event)=>{
+
+             }}>
+
+             </input>
+         </div>
+
+        </div>
+    )
+}
+export default Tempapp;
