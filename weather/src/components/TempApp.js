35e5a3a9ac1d9b6f74b67e51c78fa22e4895d69b
Himanshu <himanshu1112.cse19@chitkara.edu.in>
2022-01-30 14:37:48 +0530

    Project Done

--- weather/src/components/TempApp.js
+++ weather/src/components/TempApp.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./css/style.css";
 
-const Tempapp= ()=>{
-    return(
-        <div className="box">
-         <div className="inputData">
-             <input type="search" className="inputField" onChange={(event)=>{
+const Tempapp = () => {
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Chandigarh");
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=42d84940754c02d5885f293822ef4d29`
+            const response = await fetch(url);
+            const resJson = await response.json();
+            setCity(resJson.main);
+        }
+        fetchApi();
+    }, [search])
+    return (
+        <>
+            <div className="box">
+                <div className="inputData">
+                    <input type="search" className="inputField" onChange={(event) => {
+                        setSearch(event.target.value);
+                    }} />
+                </div>
+                {
+                    !city ? (
+                        <p>No Data Found</p>
+                    ) : (
+                        <div>
+                            <div className='info'>
+                                <h2 className="location">
+                                    <i className="fas fa-street-view"></i>{search}
+                                </h2>
+                                <h1 className="temp">
+                                    {city.temp} °C
+                                </h1>
+                                <h3 className="tempmin_max">
+                               Min : {city.temp_min} °C | Max : {city.temp_max} °C
+                                </h3>
+                            </div>
+                            <div className="wave -one"></div>
+                            <div className="wave -two"></div>
+                            <div className="wave -three"></div>
+                        </div>
+                    )
+                }
 
-             }}>
-
-             </input>
-         </div>
-
-        </div>
+            </div>
+        </>
     )
 }
 export default Tempapp;
